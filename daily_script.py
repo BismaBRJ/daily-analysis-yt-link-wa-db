58b1e2c29c76ec9768b81c2eb28bda65c570e989
BismaBRJ <bismabrj@gmail.com>
2025-09-08 15:27:29 +0700

    PySpark timestamp processing complete, didn't know they have a timestamp type wow. Had to downgrade from Java OpenJDK 24 to 17 though for SparkSession to be able to start

--- daily_script.py
+++ daily_script.py
@@ -3,53 +3,80 @@ The script to be ran every day
 """
 
 # Imports
+print("Importing libraries...")
 from pathlib import Path
 import datetime
 import sqlite3
 from pyspark.sql import types as sparktypes
+from pyspark.sql import functions as sf
 from pyspark.sql import SparkSession
+print("Imports complete")
 
 # Constants (settings, paths etc)
 DB_PATH = ( # macOS path to SQLite database file. Feel free to relocate
     Path("~") / "Library" / "Group Containers" /
     "group.net.whatsapp.WhatsApp.shared" /
     "ChatStorage.sqlite"
-)
-PHONE_NUMBERS = ["628123456789"]
+).expanduser()
+PHONE_NUMBERS = ["628123456789", "6281357986420"]
 
 # Script
 
 # 1. Connect to database
 
+print("Connecting to database...")
 if (not DB_PATH.is_file()):
-    print("Error: database not found at")
+    print("Error: database file not found at")
     print(DB_PATH)
     print("which is the path specified in the daily script.")
 
 conn = sqlite3.connect(DB_PATH)
+print("Database connected")
 
 # 2. Deal with timestamps
 
+dt_unix_epoch = datetime.datetime(
+    1970, 1, 1, 0, 0, 0,
+    tzinfo=datetime.timezone.utc
+)
+timestamp_unix_epoch = dt_unix_epoch.timestamp()
+timestamp_unix_epoch_int = int(timestamp_unix_epoch)
 dt_wa_epoch = datetime.datetime(
     2001, 1, 1, 0, 0, 0,
     tzinfo=datetime.timezone.utc
 )
 timestamp_wa_epoch = dt_wa_epoch.timestamp()
+timestamp_wa_epoch_int = int(timestamp_wa_epoch)
+
+def wa_timestamp_from_unix(unix_timestamp):
+    return (
+        unix_timestamp -
+        (timestamp_wa_epoch_int - timestamp_unix_epoch_int)
+    )
+
+def unix_timestamp_from_wa(wa_timestamp):
+    return (
+        wa_timestamp +
+        timestamp_wa_epoch_int - timestamp_unix_epoch_int
+    )
 
 date_today = datetime.date.today()
 time_midnight = datetime.time(0,0,0)
 dt_today_midnight = datetime.datetime.combine(date_today, time_midnight)
-raw_timestamp_today = dt_today_midnight.timestamp()
-adjusted_timestamp_today = int(raw_timestamp_today - timestamp_wa_epoch)
+unix_timestamp_today = dt_today_midnight.timestamp()
+wa_timestamp_today = wa_timestamp_from_unix(unix_timestamp_today)
+wa_timestamp_today_int = int(wa_timestamp_today)
 
 #date_tomorrow = date_today + datetime.timedelta(days=1)
 #dt_tomorrow_midnight = datetime.datetime.combine(date_tomorrow, time_midnight)
 dt_tomorrow_midnight = dt_today_midnight + datetime.timedelta(days=1)
-raw_timestamp_tomorrow = dt_tomorrow_midnight.timestamp()
-adjusted_timestamp_tomorrow = int(raw_timestamp_tomorrow - timestamp_wa_epoch)
+unix_timestamp_tomorrow = dt_tomorrow_midnight.timestamp()
+wa_timestamp_tomorrow = wa_timestamp_from_unix(unix_timestamp_tomorrow)
+wa_timestamp_tomorrow_int = int(wa_timestamp_tomorrow)
 
 # 3. Query data
 
+print("Querying database...")
 phone_rows_map = dict()
 for phone_str in PHONE_NUMBERS:
     cur = conn.cursor()
@@ -65,9 +92,9 @@ WHERE
     AND
     ZTOJID IS NULL
     AND
-    ZMESSAGEDATE >= {adjusted_timestamp_today}
+    ZMESSAGEDATE >= {wa_timestamp_today_int}
     AND
-    ZSENTDATE < {adjusted_timestamp_tomorrow}
+    ZSENTDATE < {wa_timestamp_tomorrow_int}
 ORDER BY ZMESSAGEDATE ASC
 LIMIT 5
 ;
@@ -75,45 +102,67 @@ LIMIT 5
     rows_list = cur.fetchall()
     phone_rows_map[phone_str] = rows_list
 
+print("Querying complete. Closing database connection...")
 conn.close()
+print("Database connection closed")
 
 # 4. Create PySpark dataframe, initial transforms
 
-rows_schema = sparktypes.StructType(
-    [
-        sparktypes.StructField(
-            name="text",
-            dataType=sparktypes.StringType(), nullable=False
-        ),
-        sparktypes.StructField(
-            name="message_timestamp",
-            dataType=sparktypes.IntegerType(), nullable=False
-        ),
-        sparktypes.StructField(
-            name="sent_timestamp",
-            dataType=sparktypes.IntegerType(), nullable=False
-        )
-    ]
+wa_rows_schema = sparktypes.StructType([
+    sparktypes.StructField(
+        name="text",
+        dataType=sparktypes.StringType(), nullable=False
+    ),
+    sparktypes.StructField(
+        name="message_timestamp",
+        dataType=sparktypes.IntegerType(), nullable=False
+    ),
+    sparktypes.StructField(
+        name="sent_timestamp",
+        dataType=sparktypes.DoubleType(), nullable=False
+    )
+])
+
+# probably not needed since it's just addition/subtraction but just in case
+udf_unix_timestamp_from_wa = sf.udf(
+    f=unix_timestamp_from_wa,
+    returnType=sparktypes.IntegerType()
 )
 
+print("Starting SparkSession...")
 spark = (
     SparkSession.builder
     .appName("DailyQuery")
     .getOrCreate()
 )
+print("SparkSession started")
 
 df_list = []
+print("Creating PySpark DataFrames...")
 for phone_number, rows_list in phone_rows_map.items():
     current_df = spark.createDataFrame(
-        data=rows_list, schema=rows_schema
-    )
+        data=rows_list, schema=wa_rows_schema
+    ).withColumns({
+        "message_timestamp":
+        (
+            unix_timestamp_from_wa(sf.col("message_timestamp"))
+            .cast("timestamp")
+        ),
+        "sent_timestamp":
+        (
+            unix_timestamp_from_wa(sf.col("sent_timestamp"))
+            .cast("timestamp")
+        )
+    })
     df_list.append(current_df)
+print("DataFrames created")
 
+# debug
+print("Displaying DataFrames...")
 for cur_df in df_list:
-    pass # todo
-
-# todo
-
-#wa_epoch = datetime(2001, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
+    print(cur_df.show())
+print("DataFrames displayed")
 
 # 5. Save "raw" data to Parquet
+
+#todo
